e6075da33e63d89279825f25845e5261d1c7a967
osman bah <osmanbah441@gmail.com>
2024-02-29 15:50:08 +0000

    initial setup

---
+++ lib/component_library.dart
@@ -0,0 +1,7 @@
+export 'src/social_media_links.dart';
+export 'src/theme/theme_data.dart';
+export 'src/service_card.dart';
+export 'src/testimonial_card.dart';
+export 'src/contact_card.dart';
+export 'src/content_card.dart';
+export 'src/logo.dart';
